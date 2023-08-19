725a9e0dcc1da604d9d93a76817b80e54492b046
submaria <marialuizaasm@gmail.com>
2023-08-18 23:38:51 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+
+
+<script setup>
+import EventCalendar from './components/EventCalendar.vue'
+</script>
+
+<template>
+  <EventCalendar />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/EventCalendar.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { reactive } from "vue";
+// import '@fullcalendar/core/vdom'
+import FullCalendar from "@fullcalendar/vue3";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
+import interactionPlugin from "@fullcalendar/interaction";
+
+const options = reactive({
+  plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
+  initialView: "dayGridMonth",
+});
+</script>
+
+<template>
+  <FullCalendar v-bind:options="options" />
+</template>
